0b58d35e36078e89f826832592bd781c934e5f62
Iconoclast-70 <phil.mh.milly@gmail.com>
2021-06-01 21:53:38 +0000

    updated typewriter

--- typewriter.js
+++ typewriter.js
@@ -1,11 +1,13 @@
-const sentence = "hello there from lighthouse labs";
-let animate = 0;
-sentence = sentence + "\n";
-for (const char of sentence) {
-  animate += 50;
-  setTimeout(() => {
-    process.stdout.write(char);
-  }, 1000 + animate);
-}; 
-
+const typewriter = function(sentence) {
+  let animate = 0;
+  sentence = sentence + "\n";
+  for (let i = 0;i < sentence.length; i++) {
+    animate += 50;
+    setTimeout(() => {
+      process.stdout.write(sentence[i]);
+    }, 1000 + animate);
+  };
+}
+ 
+typewriter("hello there from lighthouse labs");
 
